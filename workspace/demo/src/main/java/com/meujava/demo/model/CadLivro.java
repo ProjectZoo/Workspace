9dd2bef06aeb7ac362b96c141d894ed3b5c6d57a
Program <100379860+ProjectZoo@users.noreply.github.com>
2024-04-09 18:29:00 -0300

    Add files via upload

---
+++ workspace/demo/src/main/java/com/meujava/demo/model/CadLivro.java
@@ -0,0 +1,81 @@
+package com.meujava.demo.model;
+
+import java.io.Serializable;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+
+@Entity
+@Table(name="TB_LIVRO")
+public class CadLivro implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	private String titulo;	
+	private String autor;	
+	private String editora;	
+	private String assunto;	
+	private String isbn;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public String getAssunto() {
+		return assunto;
+	}
+
+	public void setAssunto(String assunto) {
+		this.assunto = assunto;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	
+	
+}
